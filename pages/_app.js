f7914e217dd988cc370b26843bb7b69c629fc8ec
mARUwTC <chriswong0621@gmail.com>
2023-01-26 20:12:33 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -12,14 +12,25 @@ import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
 
+import NProgress from 'nprogress'
+
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
+
+Router.events.on("routeChangeStart", (url)=>{
+  Nprogress.start()
+})
+
+Router.events.on("routeChangeComplete", (url)=>{
+  Nprogress.done(false)
+});
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
       </Head>
       {isDevelopment && isSocket && <ClientReload />}
       <Analytics />
