3c4be02dc8d6754326798acecc1073a078dd21c3
mARUwTC <chriswong0621@gmail.com>
2023-01-26 19:57:09 +0800

    new file:   css/loading.css 	modified:   pages/_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -12,12 +12,41 @@ import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
 
+import '../css/loading.css'
+import { useRouter } from 'next/router';
+import { useState,useEffect } from 'react'
+
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
+
+function Loading() {
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+      const handleStart = (url) => (url !== router.asPath) && setLoading(true);
+      const handleComplete = (url) => (url === router.asPath) && setTimeout(() =>{setLoading(false)},5000);
+
+      router.events.on('routeChangeStart', handleStart)
+      router.events.on('routeChangeComplete', handleComplete)
+      router.events.on('routeChangeError',  handleComplete)
+
+      return () => {
+          router.events.off('routeChangeStart', handleStart)
+          router.events.off('routeChangeComplete', handleComplete)
+          router.events.off('routeChangeError', handleComplete)
+      }
+  })
+  
+  return loading && (<div className='spinner-wrapper'>
+    <div className="spinner"></div></div>)
+}
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+      <Loading/>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
