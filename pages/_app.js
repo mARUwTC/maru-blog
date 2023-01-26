9a9e8d22c4ef278609bac2774d9ba2c0b8c51193
mARUwTC <chriswong0621@gmail.com>
2023-01-26 20:07:23 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -12,36 +12,8 @@ import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
 
-import '../css/loading.css'
-import { useRouter } from 'next/router';
-import { useState,useEffect } from 'react'
-
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
-
-function Loading() {
-  const router = useRouter();
-
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-      const handleStart = (url) => (url !== router.asPath) && setLoading(true);
-      const handleComplete = (url) => (url === router.asPath) && setTimeout(() =>{setLoading(false)},1000);
-
-      router.events.on('routeChangeStart', handleStart)
-      router.events.on('routeChangeComplete', handleComplete)
-      router.events.on('routeChangeError',  handleComplete)
-
-      return () => {
-          router.events.off('routeChangeStart', handleStart)
-          router.events.off('routeChangeComplete', handleComplete)
-          router.events.off('routeChangeError', handleComplete)
-      }
-  })
-  
-  return loading && (<div className='spinner-wrapper'>
-    <div className="spinner"></div></div>)
-}
 
 export default function App({ Component, pageProps }) {
   return (
