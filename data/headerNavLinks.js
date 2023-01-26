82ef114799d9ef787d46d123fd1515a7f339810f
mARUwTC <chriswong0621@gmail.com>
2023-01-26 16:58:52 +0800

    modified:   data/headerNavLinks.js

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/about', title: 'About' },
+  { href: '/tags', title: 'Categories' },
+  { href: '/tags/日本語', title: '日本語' },
 ]
 
 export default headerNavLinks
