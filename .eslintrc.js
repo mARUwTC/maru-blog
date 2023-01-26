211c60f39000944cb8e55b10596bbd7d1d2a1e57
mARUwTC <chriswong0621@gmail.com>
2023-01-26 15:02:02 +0800

    modified:   .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,11 +8,10 @@ module.exports = {
   },
   extends: ['eslint:recommended', 'plugin:prettier/recommended', 'next', 'next/core-web-vitals'],
   rules: {
-    'prettier/prettier': 'error',
+    'prettier/prettier': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 0,
     'no-unused-vars': 0,
-    "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off"
+    'react/no-unescaped-entities': 0,
   },
 }
