340ec2cf8741ede2245f490b816d75ffc3a8fc13
mARUwTC <chriswong0621@gmail.com>
2023-01-27 21:44:26 +0800

    modified:   components/Footer.js 	modified:   css/tailwind.css 	modified:   "data/blog/Japanese/N5\346\226\207\346\263\225\343\200\220\350\241\250\347\217\276\347\257\207\343\200\221\343\200\214\343\201\246\343\201\204\343\202\213\357\274\217\343\201\246\343\201\202\343\202\213\343\200\215.mdx" 	modified:   layouts/ListLayout.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,14 @@
 import Link from './Link'
 import siteMetadata from '@/data/siteMetadata'
 
-
 export default function Footer() {
   return (
     <footer>
-      <div className="mt-16 flex flex-col items-center">
+      <div className="my-10 flex flex-col items-center">
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
           <div>{` • `}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>
+          <div>{`© 2022 - ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
         </div>
