b6667354de9e6faa87c6bb0a25b6a1da301509f6
mARUwTC <chriswong0621@gmail.com>
2023-01-27 19:54:51 +0800

    Revert "	modified:   components/LayoutWrapper.js"
    This reverts commit 7d2938b7893f32841d099ab9e954cc81b699027e.

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,15 +7,15 @@ const siteMetadata = {
   theme: 'system', // system, dark or light
   siteUrl: 'https://www.kurisu.tk',
   siteRepo: 'https://github.com/mARUwTC/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo2.png',
-  image: '/static/images/logo2.png',
+  siteLogo: '/static/images/ico.jepg',
+  image: '/static/images/ico.jepg',
   socialBanner: '/static/images/twitter-card.png',
-  email: '/',
-  github: '/',
-  twitter: '/',
-  facebook: '/',
-  youtube: '/',
-  linkedin: '/',
+  email: 'address@yoursite.com',
+  github: 'https://github.com',
+  twitter: 'https://twitter.com/Twitter',
+  facebook: 'https://facebook.com',
+  youtube: 'https://youtube.com',
+  linkedin: 'https://www.linkedin.com',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
