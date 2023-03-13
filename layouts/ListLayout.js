8956be9d90c027ab588db6328b58e0be5c124378
mARUwTC <86964487+mARUwTC@users.noreply.github.com>
2023-03-13 16:25:17 +0800

    Update ListLayout.js

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -74,11 +74,11 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
                           ))}
                         </div>
                       </div>
-                      /*
+                      {/*
                       <div className="prose max-w-none text-gray-500 dark:text-gray-400">
                         {summary}
                       </div>
-                      */
+                      */}
                     </div>
                   </article>
                 </li>
